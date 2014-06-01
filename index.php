4eb21e7d28b11c0036a59a5d7be2c14278937a8c
courtoisL <courtois.ludovic@free.fr>
2014-06-01 21:15:08 +0200

    Ajout du listage des fichiers du mois par défaut si il y en a

--- index.php
+++ index.php
@@ -14,8 +14,59 @@
 </head>
 <body>
 	<?php 
+		/* Fonctions */
+		function dir_nav($specificFolder) {
+			global $exclude_list, $dir_path;
 
+			if ($specificFolder == "") {
+				$directories = array_diff(scandir($dir_path), $exclude_list);
+				echo "<ul>";
+				foreach($directories as $entry) {
+					if(is_dir($dir_path.$entry)) {
+						echo "<li><a href='?dir=".$_GET["dir"].$entry."/"."'>".$entry."</a></li>";
+					}
+				}
+				echo "</ul>";
+			}
+			else {
+				$files = array_diff(scandir($dir_path.$specificFolder), $exclude_list);
+				
+				if (count($files)==0) {
+					echo "Rien ce mois-ci...";
+				}
+				else{
+					echo "Les fichiers du mois";
+					echo "<ul>";
+					foreach($files as $entry) {
+						if(is_file($dir_path.$specificFolder."/".$entry)) {
+							echo "<li><a href=".$specificFolder."/".$entry.">".$entry."</a></li>";
+						}
+					}
+					echo "</ul>";
+				}
+			}
+			
+		}
+
+		function list_dir()
+		{
+
+			global $exclude_list, $dir_path, $folders;
+			$directories = array_diff(scandir($dir_path), $exclude_list);
+			foreach($directories as $entry) {
+				if(is_dir($dir_path.$entry)) {
+					array_push($folders, $entry);
+				}
+			}
+
+		}
+		/* Fin des fonctions */
+
+		/* Déclaration des variables */
 		$folders = array();
+		$todayYear = date("Y");
+		$todayMonth = date("m");
+		$todayFolderFormat = $todayYear."-".$todayMonth;
 
 		$exclude_list = array(".", "..", ".git", ".gitignore", "README.md", "index.php", ".DS_Store");
 		$install_path = "/lisf/";
@@ -26,28 +77,18 @@
 		else {
 		  $dir_path = $_SERVER["DOCUMENT_ROOT"].$install_path;
 		}
+		/* Fin de la déclaration des variables */
 
-		function dir_nav() {
-			global $exclude_list, $dir_path;
-			$directories = array_diff(scandir($dir_path), $exclude_list);
-			echo "<ul>";
-			foreach($directories as $entry) {
-				if(is_dir($dir_path.$entry)) {
-					echo "<li><a href='?dir=".$_GET["dir"].$entry."/"."'>".$entry."</a></li>";
-				}
-			}
-			echo "</ul>";
+		/* Début du programme de base */
+		dir_nav("");
+		list_dir();
 
-			echo "<ul>";
-			foreach($directories as $entry) {
-				if(is_file($dir_path.$entry)) {
-					echo "<li><a href=".$install_path.$_GET["dir"].$entry.">".$entry."</a></li>";
-				}
+		foreach ($folders as $folder) {
+			if ($todayFolderFormat == $folder) {
+				dir_nav($folder);
 			}
-			echo "</ul>";
 		}
-
-		dir_nav();
+		/* Fin du programme de base*/
 
 	 ?>
 </body>
